a189bce8234a6ba68356cdea12c68b6104555afd
Fabian Müller <fabianmueller100295@gmail.com>
2023-11-14 10:25:58 +0100

    quickstart tutorial

--- test/quickstart.jl
+++ test/quickstart.jl
@@ -1,5 +1,6 @@
 using Experiments
 using Dates
+using DataFrames
 
 # Define the first example timeseries
 t1 = DateTime(2018,1,1,0,0,0) .+ Experiments.hours2duration.(0:0.1:15)
@@ -18,3 +19,15 @@ using Plots
 p = scatter(ts1)
 scatter!(ts2)
 savefig(p, "docs/assets/quickstart_1.svg")
+
+# Create an experiment with both timeseries
+
+exp1 = Experiments.Experiment(DataFrame(ts1), DataFrame(ts2); name="Experiment1", timecol = :timestamp)
+exp1 = Experiment("Experiment1", ts1, ts2)
+
+timeseries(exp1)
+DataFrame(timeseries(exp1))
+DataFrame(exp1)
+
+timeseries(exp1, :measurement1)
+timeseries(exp1, :measurement2)
